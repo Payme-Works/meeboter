9a7e609949a20f61daf19f6b470ecd82b973f063
André "Dezzy <contact@andredezzy.com>
2025-08-30 11:45:26 -0300

    Refactor Dashboard Component for Improved Layout and Readability
    - Simplified the structure of the Dashboard component by removing unnecessary div wrappers, enhancing layout clarity.
    - Ensured consistent formatting and readability of the DashboardCard components for better user experience.
    - Maintained existing functionality while improving the overall organization of the component.

--- apps/server/src/app/_components/dashboard.tsx
+++ apps/server/src/app/_components/dashboard.tsx
@@ -36,66 +36,62 @@ export default function Dashboard() {
 				</p>
 			</div>
 
-			<div>
-				<div className="grid grid-cols-1 gap-6 lg:h-[48rem] lg:grid-cols-3">
-					<DashboardCard
-						title="Active Bots"
-						className="h-full min-h-56"
-						content={
-							activeBotCountLoading ? (
-								<Skeleton className="h-10 w-10" />
-							) : activeBotCountError ? (
-								<div className="text-4xl font-bold">
-									<ErrorAlert errorMessage={activeBotCountError.message} />
-								</div>
-							) : (
-								<div className="text-4xl font-bold">
-									{activeBotCount?.count}
-								</div>
-							)
-						}
-						icon={<Bot />}
-						link={{
-							type: "INTERNAL",
-							url: "/bots",
-							text: "View Bots",
-						}}
-					/>
+			<div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+				<DashboardCard
+					title="Active Bots"
+					className="h-full min-h-56"
+					content={
+						activeBotCountLoading ? (
+							<Skeleton className="h-10 w-10" />
+						) : activeBotCountError ? (
+							<div className="text-4xl font-bold">
+								<ErrorAlert errorMessage={activeBotCountError.message} />
+							</div>
+						) : (
+							<div className="text-4xl font-bold">{activeBotCount?.count}</div>
+						)
+					}
+					icon={<Bot />}
+					link={{
+						type: "INTERNAL",
+						url: "/bots",
+						text: "View Bots",
+					}}
+				/>
 
-					<DashboardCard
-						title="Active Keys"
-						className="h-full min-h-56"
-						content={
-							keyCountLoading ? (
-								<Skeleton className="h-10 w-10" />
-							) : keyCountError ? (
-								<div className="text-4xl font-bold">
-									<ErrorAlert errorMessage={keyCountError.message} />
-								</div>
-							) : (
-								<div className="text-4xl font-bold">{keyCount?.count}</div>
-							)
-						}
-						icon={<Key />}
-						link={{
-							type: "INTERNAL",
-							url: "/api-keys",
-							text: "View API Keys",
-						}}
-					/>
+				<DashboardCard
+					title="Active Keys"
+					className="h-full min-h-56"
+					content={
+						keyCountLoading ? (
+							<Skeleton className="h-10 w-10" />
+						) : keyCountError ? (
+							<div className="text-4xl font-bold">
+								<ErrorAlert errorMessage={keyCountError.message} />
+							</div>
+						) : (
+							<div className="text-4xl font-bold">{keyCount?.count}</div>
+						)
+					}
+					icon={<Key />}
+					link={{
+						type: "INTERNAL",
+						url: "/api-keys",
+						text: "View API Keys",
+					}}
+				/>
 
-					<DashboardCard
-						title="View our Docs"
-						className="h-full min-h-56"
-						content="To learn more about how to create bots, pull meeting recordings, pull transcriptions and more, view our Documentation!"
-						icon={<File className="text-slate-500" />}
-						link={{
-							type: "INTERNAL",
-							url: "/docs",
-							text: "View Documentation",
-						}}
-					/>
-				</div>
+				<DashboardCard
+					title="View our Docs"
+					className="h-full min-h-56"
+					content="To learn more about how to create bots, pull meeting recordings, pull transcriptions and more, view our Documentation!"
+					icon={<File className="text-slate-500" />}
+					link={{
+						type: "INTERNAL",
+						url: "/docs",
+						text: "View Documentation",
+					}}
+				/>
 			</div>
 		</div>
 	);
